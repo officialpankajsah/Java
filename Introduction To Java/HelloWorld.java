819d74a3e534c79155379a3606b5f9f580f379d6
Pankaj Sah <95707576+officialpankajsah@users.noreply.github.com>
2022-12-25 13:17:58 +0530

    Add files via upload

---
+++ Introduction To Java/HelloWorld.java
@@ -0,0 +1,18 @@
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+}
+
+
+// Q-> What is method?
+// A-> It is a collection of statements that performs an operation.
+// Q-> What is Main Method?
+// A-> Main method is a special method that Java looks for when running a program. It's called the entry point as well.
+// public -> public is the access modifier.
+// static -> static is a java keyword.
+// void -> void is also a java keyword used to indicate that the method will not return anything.
+// * The left and right parenthesis in a method declaration are mandatory and can include one or more parameters, to pass any value or info to the method.
+// code block -> code block is used to define a block of code. We must have one in the method declaration and it's where we will be adding statements to perform certain tasks.
+// statement -> statement is a command to be executed can include one or more expressions.
+
---
+++ Introduction To Java/HelloWorldAssignment.java
@@ -0,0 +1,9 @@
+public class HelloWorldAssignment {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        System.out.println("Prepinsta is Awesome");
+        System.out.println("Welcome to the Java course");
+        System.out.println("If you want to learn Java in depth, complete this playlist.");
+
+    }
+}
